76d45c19c11d59632b04ab4b55f3288a5abf7035
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2019-09-30 19:56:40 -0500

    Pequeño loin de Identity

---
+++ Shop.Test.UnitTest/ContextUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.Domain.Entities;
+using Shop.Infrastructure.EFDataContext;
+using Shop.Services.implementation;
+using Shop.Services.Interfaces.Handlers;
+using Shop.Services.Interfaces.Requests;
+
+namespace Shop.Test.UnitTest
+{
+    [TestClass]
+    public class ContextUnitTest
+    {
+        [TestMethod]
+        public void CategoryInsertionTest()
+        {
+            using (var db = new ShopDb())
+            {
+                Category newCatery = new Category()
+                {
+                    Name = "Frutas",
+                    Description = "Vegetales dulces"
+                };
+                db.Categories.Add(newCatery);
+                db.SaveChanges();
+                Assert.IsTrue(newCatery.Id>0);
+            }
+        }
+
+        [TestMethod]
+        public void CreateANewProduct()
+        {
+            IProductHandler productHandler = new EFProductHandler();
+            var newProduct = productHandler.RegisterProduct(new CreateProducto()
+            {
+                CategoryId = 1,
+                Descripcion = "Es roja",
+                Name = "Fresa"
+            });
+            Assert.AreEqual(newProduct.CategoryName, "Frutas");
+            Assert.IsTrue(newProduct.Id > 0);
+            Assert.AreEqual(newProduct.Name, "Fresa");
+            Assert.AreEqual(newProduct.Description, "Es roja");
+        }
+    }
+}
---
+++ Shop.Test.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shop.Test.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shop.Test.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00c1c6d4-bc4f-4f32-9f28-ae9148fbaba8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
